8a02657beec8e82bbc12d271b94d042864189c95
Keturah Howard <keturah.dev@gmail.com>
2020-05-15 17:08:12 -0700

    relavant changes to previous commit

---
+++ MERN/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser'); //bring in dependencies
+
+const app = express(); // still unsure
+
+// mongoose.Promise = global.Promise; //hm...
+// mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost:27017/node-react-starter`);
